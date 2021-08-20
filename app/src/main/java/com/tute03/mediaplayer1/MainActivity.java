f8ab200e0daf73fd8a059eaeff5cfd5b838f70d5
starshooter12345 <80570233+starshooter12345@users.noreply.github.com>
2021-08-20 20:51:52 +0530

    initial

---
+++ app/src/main/java/com/tute03/mediaplayer1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tute03.mediaplayer1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void play(View view){
+        if(mediaPlayer==null){
+            mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.song);
+            mediaPlayer.start();
+        }else{
+            mediaPlayer.start();
+
+        }
+
+    }
+    public void pause(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.pause();
+        }
+
+    }
+    public void stop(View view){
+        mediaPlayer.release();
+        mediaPlayer=null;
+
+    }
+
+}
